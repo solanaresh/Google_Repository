d323e3d56907694d699d7af36fbdd19ebbbe4f68
Naresh <Naresh>
2019-10-06 23:36:43 +0530

    Sample Change Comment Added

--- src/main/java/com/tutorialspoint/projects/googleservice/GoogleserviceApplication.java
+++ src/main/java/com/tutorialspoint/projects/googleservice/GoogleserviceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//sample project
 @SpringBootApplication
 @ComponentScan(basePackages = {
 "com.tutorialspoint.projects.googleservice.config"})
